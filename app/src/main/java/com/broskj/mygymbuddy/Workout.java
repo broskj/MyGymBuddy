ba3411d73a9f6a7b33b609946374134893007137
broskj <kjbrost@gmail.com>
2015-06-28 21:02:20 -0400

    added date variable used to measure time between current time and last completed workout

--- app/src/main/java/com/broskj/mygymbuddy/Workout.java
+++ app/src/main/java/com/broskj/mygymbuddy/Workout.java
@@ -10,17 +10,26 @@ import java.util.ArrayList;
 public class Workout {
     String name;
     ArrayList<Exercise> exercises;
+    long date;
 
     Workout() {
         this.name = "";
+        this.exercises = new ArrayList<>();
+        this.date = -1;
     }//end default constructor
 
     Workout(String _name) {
         this.name = _name.toUpperCase();
+        this.exercises = new ArrayList<>();
+        this.date = -1;
     }//end constructor
 
     public void addExercise(Exercise exercise) {
         exercises.add(exercise);
+    }
+
+    public void completeWorkout() {
+        this.date = System.currentTimeMillis();
     }
 
 }//end class Workout
