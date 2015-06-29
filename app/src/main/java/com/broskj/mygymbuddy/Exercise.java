8997cd55952b4faaf9a46216dd4233eba8b61029
broskj <kjbrost@gmail.com>
2015-06-28 20:53:05 -0400

    added/altered variables for weightlifting versions of exercise, added new constructors and methods

--- app/src/main/java/com/broskj/mygymbuddy/Exercise.java
+++ app/src/main/java/com/broskj/mygymbuddy/Exercise.java
@@ -1,7 +1,5 @@
 package com.broskj.mygymbuddy;
 
-import javax.xml.datatype.Duration;
-
 /**
  * Created by Kyle on 6/24/2015.
  * <p/>
@@ -11,37 +9,57 @@ import javax.xml.datatype.Duration;
  */
 
 public class Exercise {
-    final int TYPE_CARDIO = 0;
-    final int TYPE_LIFT = 1;
+    public final int TYPE_CARDIO = 0;
+    public final int TYPE_LIFT = 1;
     String name; //exercise name, e.g. 'Leg Day' or '5x5 B'
     boolean increment; //whether or not the user wants to increment weight/time each workout
+    boolean repScheme; //whether or not the user is following a rep scheme (10-8-6)
+    boolean weightScheme; //whether or not the user is following a weight scheme (135-155-185)
     int type; //either 0 or 1, maybe more in the future
     int time; //measured in seconds, converted into hours:minutes;seconds
     int incrementTime; //measured in seconds, but typically used for adding minutes to 'time'
     int sets; //number of sets per workout
-    int[] reps; //number of reps per set.  size initialized to sets, and
-    double weight; //amount of initial weight, incrementable
+    int[] reps; //number of reps per set.  size initialized to sets
+    double weight[]; //amount of initial weight, incrementable
     double incrementWeight; //amount of weight to be added after each successful lift
 
     Exercise() {
-
+        this.name = "";
     }//end default constructor
 
-    Exercise(String _name, boolean _increment, int _type, int _time, int _sets, int[] _reps,
-             double _weight, int _incrementTime, double _incrementWeight) {
+    Exercise(String _name, boolean _increment, int _type, int _time, int _incrementTime) {
         this.name = _name.toUpperCase();
         this.increment = _increment;
         this.type = _type;
         this.time = _time;
+        this.incrementTime = _incrementTime;
+    }//end cardio constructor
+
+    Exercise(String _name, boolean _increment, boolean _repScheme, boolean _weightScheme, int _type, int _sets, int[] _reps,
+             double _weight[], double _incrementWeight) {
+        this.name = _name.toUpperCase();
+        this.increment = _increment;
+        this.repScheme = _repScheme;
+        this.weightScheme = _weightScheme;
+        this.type = _type;
         this.sets = _sets;
         this.reps = _reps;
         this.weight = _weight;
-        this.incrementTime = _incrementTime;
         this.incrementWeight = _incrementWeight;
-    }//end constructor
+    }//end weightlifting constructor
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getType() {
+        return this.type;
+    }
 
     public void incrementWeight() {
-        weight += incrementWeight;
+        weight[0] += incrementWeight;
+        for (int i = 1; i < weight.length; i++)
+            weight[i] += incrementWeight;
     }//end incrementWeight
 
     public void incrementTime() {
